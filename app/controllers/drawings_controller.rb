d3910d61b600bdf024d548c6ce31ca648aa58d5e
giuliacajati <giuliacajati@gmail.com>
2021-04-19 13:08:58 -0400

    added relationship to index

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -2,7 +2,7 @@ class DrawingsController < ApplicationController
     
     def index 
         @drawings = Drawing.all
-        render  json: @drawings.to_json
+        render  json: @drawings.to_json(include: [:markers])   
     end 
 
 end
