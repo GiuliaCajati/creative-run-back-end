2f9535fdd2bd19293df5ea405ed506b0e8249d05
giuliacajati <giuliacajati@gmail.com>
2021-04-22 10:29:21 -0400

    added routing methods to ,markers model

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,10 +1,28 @@
 class Marker < ApplicationRecord
     belongs_to :drawing
+    # url = 'http://127.0.0.1:5000/route/v1/foot/'
 
-    # add a method... once a "end marker" is added a "route" created between the twor markers 
-    # 
+    # def find_connecting_markers
+    #     current_marker = self.marker_id
+    #     if current_marker.place > 1 
+    #         prev_marker = Marker.where(drawing_id: test.drawing_id, place: test.place+1)
+    #         start = [  prev_marker.longitude , prev_marker.latitude ]
+    #         finish = [ current_marker.longitude , current_marker.latitude ]
+    #         add_route(start, finish)
+    # end
 
-    url = 'http://127.0.0.1:5000/route/v1/foot/13.388860,52.517037;13.385983,52.496891?steps=true'
+    # def add_route(start, finish) 
+    #     coordinates = start[0], start[1]; finish[0], finish[1]  
+    #     # fetch route 
+    #     url + coordinates.join(',')
+    # end
+
+
 
     
+    
+    # 13.388860,52.517037;13.385983,52.496891?steps=true'
+    # /{service}/{version}/{profile}/{coordinates}[.{format}]?option=value&option=value
+
+
 end
