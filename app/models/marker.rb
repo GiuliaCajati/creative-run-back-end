491821bdf0698620b5b103eb21457a5593ffcf63
giuliacajati <giuliacajati@gmail.com>
2021-04-22 10:47:26 -0400

    sudokode to add routes

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,6 +1,16 @@
+require 'rest-client'
+require 'json'
 class Marker < ApplicationRecord
     belongs_to :drawing
+
+#####Fetch data
     # url = 'http://127.0.0.1:5000/route/v1/foot/'
+    # run server 
+    #docker run -t -i -p 5000:5000 -v "${PWD}:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/district-of-columbia-latest.osrm
+    # 13.388860,52.517037;13.385983,52.496891?steps=true'
+    # /{service}/{version}/{profile}/{coordinates}[.{format}]?option=value&option=value
+#####################################################################
+
 
     # def find_connecting_markers
     #     current_marker = self.marker_id
@@ -8,21 +18,15 @@ class Marker < ApplicationRecord
     #         prev_marker = Marker.where(drawing_id: test.drawing_id, place: test.place+1)
     #         start = [  prev_marker.longitude , prev_marker.latitude ]
     #         finish = [ current_marker.longitude , current_marker.latitude ]
+    #   might need to round start and finish 
     #         add_route(start, finish)
     # end
 
     # def add_route(start, finish) 
-    #     coordinates = start[0], start[1]; finish[0], finish[1]  
-    #     # fetch route 
-    #     url + coordinates.join(',')
+    #   coordinates = start[0], start[1]; finish[0], finish[1]  
+    #   response = RestClient.get(url + coordinates.join(','))
+    #   JSON.parse(response)
     # end
-
-
-
-    
-    
-    # 13.388860,52.517037;13.385983,52.496891?steps=true'
-    # /{service}/{version}/{profile}/{coordinates}[.{format}]?option=value&option=value
 
 
 end
