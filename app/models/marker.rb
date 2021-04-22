9cfe268fb582618528b5b135213c729d5d9b879f
giuliacajati <giuliacajati@gmail.com>
2021-04-22 12:43:11 -0400

    json code completed

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,14 +1,14 @@
-require 'rest-client'
-require 'json'
+# require 'rest-client'
+# require 'json'
 class Marker < ApplicationRecord
     belongs_to :drawing
 
 #####Fetch data
     # url = 'http://127.0.0.1:5000/route/v1/foot/'
     # run server 
-    #docker run -t -i -p 5000:5000 -v "${PWD}:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/district-of-columbia-latest.osrm
-    # 13.388860,52.517037;13.385983,52.496891?steps=true'
-    # /{service}/{version}/{profile}/{coordinates}[.{format}]?option=value&option=value
+        #  docker run -t -i -p 5000:5000 -v "${PWD}:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/district-of-columbia-latest.osrm
+    # DEMO: https://router.project-osrm.org/route/v1/foot/13.388860,52.517037;13.385983,52.496891?steps=true&alternatives=true
+    # /route/v1/{profile}/{coordinates}?alternatives={true|false|number}&steps={true|false}&geometries={polyline|polyline6|geojson}&overview={full|simplified|false}&annotations={true|false}
 #####################################################################
 
 
@@ -24,9 +24,11 @@ class Marker < ApplicationRecord
 
     # def add_route(start, finish) 
     #   coordinates = start[0], start[1]; finish[0], finish[1]  
-    #   response = RestClient.get(url + coordinates.join(','))
+    #   response = RestClient.get(url + coordinates.join(',') + '?steps=true&alternatives=true')
     #   JSON.parse(response)
+    #polyline = OBJECT.routes[0].legs[0].steps.map(step => step.maneuver.location)
     # end
 
 
 end
+
