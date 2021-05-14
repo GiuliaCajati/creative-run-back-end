8e1cdeb8eb4b181edb0282ca77c7ae792f75b237
giuliacajati <giuliacajati@gmail.com>
2021-05-14 10:05:45 -0400

    marker controller refactoring

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -27,29 +27,3 @@ class Marker < ApplicationRecord
     end
 
 end
-
-    # DEMO: https://router.project-osrm.org/route/v1/foot/13.388860,52.517037;13.385983,52.496891?steps=true&alternatives=true
-    # def connect_all_markers
-    #     byebug 
-    #     url = 'http://127.0.0.1:5000/route/v1/foot/'
-    #     polylines = []
-    #     markers = self.all
-    #     markers.each_with_index do | m, i | 
-    #         if markers[i].place > 1 
-    #             start = [markers[i-1].longitude, markers[i-1].latitude]
-    #             finish = [markers[i].longitude, markers[i].latitude]
-    #             url_start = start.join(',') 
-    #             url_finish = finish.join(',') 
-    #             url_coordinates = url_start + ';' + url_finish
-    #             step_request = url + url_coordinates + '?steps=true&alternatives=true'
-   
-    #             data = RestClient.get step_request
-    #             polylines.push(JSON.parse(data)["routes"][0]['legs'][0]['steps'][0]['intersections'].map{|int| int['location'] })
-    #         else 
-    #             next 
-    #         end 
-    #     end
-    #     byebug 
-        
-    #     return polylines
-    # end
